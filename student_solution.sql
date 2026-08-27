9e82d53597e308ac12913941de6abe154c6ff8d2
SRIPOOVEEGASELVAM V <spvselvam123@gmail.com>
2026-08-27 10:29:58 +0530

    Update student_solution.sql

--- student_solution.sql
+++ student_solution.sql
@@ -1,1 +1,7 @@
+INSERT INTO student(StudentID, StudentName,Gender, DepartmentID)
+VALUES
+(1001, 'Arun', 'Male', 101),
+(1002, 'Divya', 'Female', 102),
+(1003, 'Karthik', 'Male', 101);
+SELECT * FROM student;
 
